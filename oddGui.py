ce74187ecd70c4b841a4af376dbbc20fc92df360
kjempire9 <kjempirecode@gmail.com>
2022-09-05 19:29:36 +0300

    darkamber theme

--- oddGui.py
+++ oddGui.py
@@ -1,11 +1,17 @@
 import PySimpleGUI as sg
+from odd import decider
+
+# The theme of the gui
+sg.theme("DarkAmber")
+
 
 
 # Layout matrix
 layout = [
     [sg.Text("Number")],
+    [sg.Input(key="msemo")],
     [sg.Button("Check")],
-    []
+    [sg.Button("Exit")],
 ]
 
 # The window definition
@@ -13,11 +19,26 @@ window = sg.Window("Odd or Even",layout)
 
 # Event loop
 
+n = 11
+
 while True:
     event, values = window.read()
 
-    if event == sg.WIN_CLOSED:
+    # Closing the window options
+    if event == "Check":
+        try:
+            fin = decider(int(values["msemo"]))
+            print(fin)
+        except ValueError:
+            print("Type a valid integer!!!") 
+
+    if event == sg.WIN_CLOSED or event == "Exit":
         break
 
-
+# Closing the window when loop is broken
 window.close()
+
+
+'''
+Wakatime has not yet worked on VScode but I hope it will
+'''
