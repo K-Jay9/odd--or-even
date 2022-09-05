ff53d7ee1a49a78194a4f5fb15f1565db8f0580b
kjempire9 <kjempirecode@gmail.com>
2022-09-05 21:27:07 +0300

    fonts and layout modification

--- oddGui.py
+++ oddGui.py
@@ -5,13 +5,19 @@ from odd import decider
 sg.theme("DarkAmber")
 
 
+#To preview all available themes
+# sg.theme_previewer()
+
 
 # Layout matrix
 layout = [
-    [sg.Text("Number")],
-    [sg.Input(key="msemo")],
-    [sg.Button("Check")],
-    [sg.Button("Exit")],
+    [sg.T(' '  * 5),sg.Text("Type the Integer:", font=("Serif", 14))],
+    [sg.T(' '  * 15)],
+    [sg.Input(key="msemo", size=(30,15))],
+    [sg.T(' '  * 15)],
+    [sg.T(' '  * 15),sg.Button("Check", font='Serif')],
+    [sg.T(' '  * 15)],
+    [sg.T(' '  * 10),sg.Text('', size=(10, 5), font=('Serif'), justification='center', key='output')]
 ]
 
 # The window definition
@@ -26,11 +32,21 @@ while True:
 
     # Closing the window options
     if event == "Check":
+        # Catching non integer entered by the user
         try:
+            # Return output from the engine
             fin = decider(int(values["msemo"]))
-            print(fin)
+
+            # Update display with different colors
+            if fin == "ODD!":
+                window["output"].update(fin, text_color='gray')
+            else:
+                window["output"].update(fin, text_color='green')
+
+
         except ValueError:
-            print("Type a valid integer!!!") 
+              # Update the display
+            window["output"].update("Type a valid integer!!!", text_color='red')
 
     if event == sg.WIN_CLOSED or event == "Exit":
         break
